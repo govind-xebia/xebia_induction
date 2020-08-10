c9b42ac5347f367c356295f8c59934cdc6873525
Govindan Gopikrishnan <govindan.gopikrishnan@tesco.com>
2020-08-10 21:26:47 +0530

    Updates

---
+++ precache-manifest.c43bf099173eea38f97b9b9fd68ab84b.js
@@ -0,0 +1,430 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8062d8eafee9f9a003a76fce62aede97",
+    "url": "/xebia_induction/index.html"
+  },
+  {
+    "revision": "f35768db89a6b62f151d",
+    "url": "/xebia_induction/static/css/2.b56a1793.chunk.css"
+  },
+  {
+    "revision": "f8fd298e534e802e59d7",
+    "url": "/xebia_induction/static/css/main.8590d1d0.chunk.css"
+  },
+  {
+    "revision": "f35768db89a6b62f151d",
+    "url": "/xebia_induction/static/js/2.ceadaf3d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/xebia_induction/static/js/2.ceadaf3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8fd298e534e802e59d7",
+    "url": "/xebia_induction/static/js/main.97a0519f.chunk.js"
+  },
+  {
+    "revision": "b4b85f978c4065306457",
+    "url": "/xebia_induction/static/js/runtime-main.1be1a2de.js"
+  },
+  {
+    "revision": "de99e9420c49a5cbcaceefb5c55e0b6b",
+    "url": "/xebia_induction/static/media/adp.de99e942.png"
+  },
+  {
+    "revision": "ad1cca31b7f374270271cdf849cd1d7d",
+    "url": "/xebia_induction/static/media/agile.ad1cca31.png"
+  },
+  {
+    "revision": "da48a56c45ac101361f620a939d24c44",
+    "url": "/xebia_induction/static/media/ajaykabra.da48a56c.jpg"
+  },
+  {
+    "revision": "4490666562fac9725916b79ff29ab293",
+    "url": "/xebia_induction/static/media/anand.44906665.png"
+  },
+  {
+    "revision": "c17ce72ed778a0bcfb0bf5f8c67f8ad5",
+    "url": "/xebia_induction/static/media/anurag.c17ce72e.png"
+  },
+  {
+    "revision": "257df14f943b5a56712286fb275d7556",
+    "url": "/xebia_induction/static/media/ashok.257df14f.png"
+  },
+  {
+    "revision": "e0397ffcf27e4bb8f291ef0834b5ba8a",
+    "url": "/xebia_induction/static/media/brijesh.e0397ffc.jpg"
+  },
+  {
+    "revision": "e8acb9d9d09394001432a3ad31893fdf",
+    "url": "/xebia_induction/static/media/calendar.e8acb9d9.svg"
+  },
+  {
+    "revision": "ed3fb2aa5e4d6a0640a519f45a1dc799",
+    "url": "/xebia_induction/static/media/cares.ed3fb2aa.svg"
+  },
+  {
+    "revision": "f8253378cd9e7e3791175327ef61f7cf",
+    "url": "/xebia_induction/static/media/client-12.f8253378.png"
+  },
+  {
+    "revision": "22edc37b3d4ca713a66f23973844e621",
+    "url": "/xebia_induction/static/media/client-13.22edc37b.png"
+  },
+  {
+    "revision": "401525cfa5234e2fe45a5115c8b01a23",
+    "url": "/xebia_induction/static/media/client-14.401525cf.png"
+  },
+  {
+    "revision": "a830a0b7ee30c3e5d95132c320c84cf8",
+    "url": "/xebia_induction/static/media/client-15.a830a0b7.png"
+  },
+  {
+    "revision": "0acfaddb042534b89d5d2a59ab0c69e8",
+    "url": "/xebia_induction/static/media/client-16.0acfaddb.png"
+  },
+  {
+    "revision": "7fc89675e77819e187eceb297e25ebd8",
+    "url": "/xebia_induction/static/media/client-17.7fc89675.png"
+  },
+  {
+    "revision": "8ba21bbd435bb7ea9f6072338ad248b2",
+    "url": "/xebia_induction/static/media/client-18.8ba21bbd.png"
+  },
+  {
+    "revision": "4a7e24c6fb7526433541723ac6854b5f",
+    "url": "/xebia_induction/static/media/client-19.4a7e24c6.png"
+  },
+  {
+    "revision": "7fc89675e77819e187eceb297e25ebd8",
+    "url": "/xebia_induction/static/media/client-2.7fc89675.png"
+  },
+  {
+    "revision": "4449c45aa84d47553b8cb5188b95fbaf",
+    "url": "/xebia_induction/static/media/client-20.4449c45a.png"
+  },
+  {
+    "revision": "053fdcf87d5cec0e66393ff1ea2051c3",
+    "url": "/xebia_induction/static/media/client-21.053fdcf8.png"
+  },
+  {
+    "revision": "f9a923da8d93e720e821a8e33f78cb81",
+    "url": "/xebia_induction/static/media/client-23.f9a923da.png"
+  },
+  {
+    "revision": "58032984ab9f168075e533200e4dbba5",
+    "url": "/xebia_induction/static/media/client-24.58032984.png"
+  },
+  {
+    "revision": "fa7ba0946c3f7fb29a6a7e601c98781b",
+    "url": "/xebia_induction/static/media/client-26.fa7ba094.png"
+  },
+  {
+    "revision": "c82d174f5762d4a25b45eb9df636c984",
+    "url": "/xebia_induction/static/media/client-27.c82d174f.png"
+  },
+  {
+    "revision": "826273e4f0ce8a3c9af0ab50a5972d6f",
+    "url": "/xebia_induction/static/media/client-28.826273e4.jpg"
+  },
+  {
+    "revision": "4def1d3c1c52fb32fa9a38b925c64a1d",
+    "url": "/xebia_induction/static/media/client-29.4def1d3c.png"
+  },
+  {
+    "revision": "759eb72fb3eae44e1d524c3b6d6dc7a8",
+    "url": "/xebia_induction/static/media/client-3.759eb72f.png"
+  },
+  {
+    "revision": "2e26c789f5604fc95e085c8ed1a0ca14",
+    "url": "/xebia_induction/static/media/client-30.2e26c789.jpg"
+  },
+  {
+    "revision": "81404c8fd6230fb5add40b994c44f6f2",
+    "url": "/xebia_induction/static/media/client-4.81404c8f.png"
+  },
+  {
+    "revision": "d8bfe5c745e94bc84e17ddc6f64727df",
+    "url": "/xebia_induction/static/media/client-9.d8bfe5c7.png"
+  },
+  {
+    "revision": "2fe81bd7fc968cceebd5e409a798932b",
+    "url": "/xebia_induction/static/media/connection.2fe81bd7.svg"
+  },
+  {
+    "revision": "9d3c1bdd266c6471db12ab70ccd65c7a",
+    "url": "/xebia_induction/static/media/crux.9d3c1bdd.png"
+  },
+  {
+    "revision": "80f6974598680347378b2bbdee805bdf",
+    "url": "/xebia_induction/static/media/customer.80f69745.svg"
+  },
+  {
+    "revision": "75eec6e3846f51435ebe8eb60685bf36",
+    "url": "/xebia_induction/static/media/data-driven.75eec6e3.svg"
+  },
+  {
+    "revision": "229164ba66ce9ccf262efb0a5acba476",
+    "url": "/xebia_induction/static/media/deepak.229164ba.jpg"
+  },
+  {
+    "revision": "ea8606a86971ae1e5ebce4f4063cd439",
+    "url": "/xebia_induction/static/media/dev theory.ea8606a8.png"
+  },
+  {
+    "revision": "d631560b438ba2d05f628b17333a524b",
+    "url": "/xebia_induction/static/media/diamond.d631560b.svg"
+  },
+  {
+    "revision": "66eeab741bd3553f5d614bd60c62c457",
+    "url": "/xebia_induction/static/media/dkm.66eeab74.png"
+  },
+  {
+    "revision": "387dcdb0d808064007b8a88237c6e6e6",
+    "url": "/xebia_induction/static/media/dots.387dcdb0.svg"
+  },
+  {
+    "revision": "63008757ca8d703c4c30e50e0b3afeb3",
+    "url": "/xebia_induction/static/media/dots.63008757.png"
+  },
+  {
+    "revision": "9f09687dbc29a82024ff127a35b7123b",
+    "url": "/xebia_induction/static/media/gallery-1.9f09687d.jpg"
+  },
+  {
+    "revision": "32ad1408e48ef805efeca368605a9e21",
+    "url": "/xebia_induction/static/media/gallery-10.32ad1408.jpg"
+  },
+  {
+    "revision": "8fd30d41d528c71ef6518001c8044ae6",
+    "url": "/xebia_induction/static/media/gallery-11.8fd30d41.jpg"
+  },
+  {
+    "revision": "34247dc7abcd29206d71b50ce9a8ad1f",
+    "url": "/xebia_induction/static/media/gallery-12.34247dc7.jpg"
+  },
+  {
+    "revision": "77265dd873f6b8384406d74b4b3cbf3b",
+    "url": "/xebia_induction/static/media/gallery-13.77265dd8.jpg"
+  },
+  {
+    "revision": "cbc051757f7eb1df4b4831757b49bad9",
+    "url": "/xebia_induction/static/media/gallery-14.cbc05175.jpg"
+  },
+  {
+    "revision": "bd464cfbe42707a018d27d3b23d850be",
+    "url": "/xebia_induction/static/media/gallery-15.bd464cfb.jpg"
+  },
+  {
+    "revision": "2894d17c87607dda3ff83c0d21206e7d",
+    "url": "/xebia_induction/static/media/gallery-16.2894d17c.jpg"
+  },
+  {
+    "revision": "15f1684d3ddc9310515064ad8b669c48",
+    "url": "/xebia_induction/static/media/gallery-17.15f1684d.jpg"
+  },
+  {
+    "revision": "a4a208f7b3d1309a5f9622c2240d3bb3",
+    "url": "/xebia_induction/static/media/gallery-18.a4a208f7.jpg"
+  },
+  {
+    "revision": "2deebad84adb6b8ea096caee0a68db9e",
+    "url": "/xebia_induction/static/media/gallery-19.2deebad8.jpg"
+  },
+  {
+    "revision": "b931b337708793ac7ea0b4e30308d7d8",
+    "url": "/xebia_induction/static/media/gallery-2.b931b337.jpg"
+  },
+  {
+    "revision": "39dc3c9404695f05abb8eb8d8c012b39",
+    "url": "/xebia_induction/static/media/gallery-20.39dc3c94.jpg"
+  },
+  {
+    "revision": "28279f2c510f9deaa3301f6f9a091da7",
+    "url": "/xebia_induction/static/media/gallery-21.28279f2c.jpg"
+  },
+  {
+    "revision": "b61af3ee12ebcb9ea446325b54084dd5",
+    "url": "/xebia_induction/static/media/gallery-22.b61af3ee.jpg"
+  },
+  {
+    "revision": "6f57dab26c28c771c3120e71e5942da7",
+    "url": "/xebia_induction/static/media/gallery-23.6f57dab2.jpg"
+  },
+  {
+    "revision": "4df1e7661a4f65434cd2f3ca708eab1d",
+    "url": "/xebia_induction/static/media/gallery-24.4df1e766.jpg"
+  },
+  {
+    "revision": "f681f240ea32f0bdaf83f9846ae7907c",
+    "url": "/xebia_induction/static/media/gallery-25.f681f240.jpg"
+  },
+  {
+    "revision": "be4a6f84101d94d703de9a68f08efca0",
+    "url": "/xebia_induction/static/media/gallery-3.be4a6f84.jpg"
+  },
+  {
+    "revision": "80183781176eff66bef86d2c071de58d",
+    "url": "/xebia_induction/static/media/gallery-4.80183781.jpg"
+  },
+  {
+    "revision": "dba21b1987b5d0b503e8cb10fd31f79b",
+    "url": "/xebia_induction/static/media/gallery-5.dba21b19.jpg"
+  },
+  {
+    "revision": "2595387bdf9052855bd271e910e6715c",
+    "url": "/xebia_induction/static/media/gallery-6.2595387b.jpg"
+  },
+  {
+    "revision": "897f45222bd9ff18ca388cbafe024fd2",
+    "url": "/xebia_induction/static/media/gallery-7.897f4522.jpg"
+  },
+  {
+    "revision": "73d37ea2a31029e8e4a746f0f7ab12dd",
+    "url": "/xebia_induction/static/media/gallery-8.73d37ea2.jpg"
+  },
+  {
+    "revision": "b9096cbf07e8754c3608c1772e8e37ab",
+    "url": "/xebia_induction/static/media/gallery-9.b9096cbf.jpg"
+  },
+  {
+    "revision": "86cb6f5d4ddd2b4d50a1db86cc7b33f2",
+    "url": "/xebia_induction/static/media/gallery.86cb6f5d.svg"
+  },
+  {
+    "revision": "9d98681e21cfd5664a7422000186476b",
+    "url": "/xebia_induction/static/media/github.9d98681e.png"
+  },
+  {
+    "revision": "efe84bb276e4bf113f44779cacd1f007",
+    "url": "/xebia_induction/static/media/glory.efe84bb2.jpg"
+  },
+  {
+    "revision": "c04a05d8f2e319e117fb6aabc61f4968",
+    "url": "/xebia_induction/static/media/hourglass.c04a05d8.svg"
+  },
+  {
+    "revision": "6c74aa89ef6a14ff331b36c74af71757",
+    "url": "/xebia_induction/static/media/jira.6c74aa89.png"
+  },
+  {
+    "revision": "15dbff583c5ed8f0a5452274fd126b27",
+    "url": "/xebia_induction/static/media/jyot.15dbff58.jpg"
+  },
+  {
+    "revision": "fe710dc47134eda118a3abd40beadc65",
+    "url": "/xebia_induction/static/media/knowledge.fe710dc4.svg"
+  },
+  {
+    "revision": "358b45156ba2be9e371ee27f7ae1934f",
+    "url": "/xebia_induction/static/media/madhur.358b4515.png"
+  },
+  {
+    "revision": "34f2e4f146ea0de7c978e99e8ae1da2d",
+    "url": "/xebia_induction/static/media/map.34f2e4f1.svg"
+  },
+  {
+    "revision": "928023ff3989615feaa18362854f1afc",
+    "url": "/xebia_induction/static/media/outlook.928023ff.png"
+  },
+  {
+    "revision": "49af62d08b99e78c42a233e296a0cf3c",
+    "url": "/xebia_induction/static/media/party.49af62d0.png"
+  },
+  {
+    "revision": "f19b7b8579490193bab46bfe8f00facd",
+    "url": "/xebia_induction/static/media/people.f19b7b85.svg"
+  },
+  {
+    "revision": "0713eb2638f998121feabf01fd7bed8d",
+    "url": "/xebia_induction/static/media/play.0713eb26.svg"
+  },
+  {
+    "revision": "7beeb8ef4ef7ff8d18fec02d10558856",
+    "url": "/xebia_induction/static/media/policy.7beeb8ef.png"
+  },
+  {
+    "revision": "96aa17f8da82d0809bc72fda1ec6097a",
+    "url": "/xebia_induction/static/media/quality.96aa17f8.svg"
+  },
+  {
+    "revision": "53a7014956adf170c83e44516dd8e741",
+    "url": "/xebia_induction/static/media/ram.53a70149.png"
+  },
+  {
+    "revision": "7ff75fc026f6f32bcc69cf1fa4d56dc8",
+    "url": "/xebia_induction/static/media/shankar.7ff75fc0.jpg"
+  },
+  {
+    "revision": "ff44daa50d131755d558bb2c9bda23ba",
+    "url": "/xebia_induction/static/media/shekhargulati.ff44daa5.jpg"
+  },
+  {
+    "revision": "62de991ec8f634fd5ceaa33036a71425",
+    "url": "/xebia_induction/static/media/smiley.62de991e.svg"
+  },
+  {
+    "revision": "791ba2a22c01d885b22f9036dbc696f0",
+    "url": "/xebia_induction/static/media/stack-state.791ba2a2.svg"
+  },
+  {
+    "revision": "a662b8b8f1a9c7763c266661b0f43147",
+    "url": "/xebia_induction/static/media/sun-umbrella.a662b8b8.svg"
+  },
+  {
+    "revision": "5be9fe83773d9da2844d5fb3b87a1f8d",
+    "url": "/xebia_induction/static/media/tallie.5be9fe83.jpg"
+  },
+  {
+    "revision": "d7113c5968d44ba450f308f2994d3045",
+    "url": "/xebia_induction/static/media/teams.d7113c59.png"
+  },
+  {
+    "revision": "2f3324a0523603c56e2984f90211a591",
+    "url": "/xebia_induction/static/media/trophy.2f3324a0.svg"
+  },
+  {
+    "revision": "cbf6ead464cf9c8593bf4ec3f1943a73",
+    "url": "/xebia_induction/static/media/vigit.cbf6ead4.jpg"
+  },
+  {
+    "revision": "58c05502813f3e2614e14c6ca831212e",
+    "url": "/xebia_induction/static/media/vikasmittal.58c05502.jpg"
+  },
+  {
+    "revision": "ee7db2588ea0209859b12ee6be1cd421",
+    "url": "/xebia_induction/static/media/vipul.ee7db258.png"
+  },
+  {
+    "revision": "f41cbefa0820e2ba6b49c3a345806862",
+    "url": "/xebia_induction/static/media/welcome.f41cbefa.png"
+  },
+  {
+    "revision": "c740b031ec0b683e913113c21add2658",
+    "url": "/xebia_induction/static/media/worldwide.c740b031.svg"
+  },
+  {
+    "revision": "00240853cf71bfb58367d747cbe35824",
+    "url": "/xebia_induction/static/media/xebiaLogo.00240853.svg"
+  },
+  {
+    "revision": "862f0d4ff58aff5007e6a0c135391ee7",
+    "url": "/xebia_induction/static/media/xebithon.862f0d4f.png"
+  },
+  {
+    "revision": "59a99400d3cf873d5e91af683fe8a5de",
+    "url": "/xebia_induction/static/media/xenie.59a99400.png"
+  },
+  {
+    "revision": "dc08ea8d2363b9cc475f2c3b77d054d6",
+    "url": "/xebia_induction/static/media/xke.dc08ea8d.png"
+  },
+  {
+    "revision": "aadb6bcd06eac108b481d45ef06607ca",
+    "url": "/xebia_induction/static/media/xspirit.aadb6bcd.svg"
+  },
+  {
+    "revision": "78a3a5baf14fd3cef65ca2347f2f8f08",
+    "url": "/xebia_induction/static/media/zoho.78a3a5ba.png"
+  }
+]);
