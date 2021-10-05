39025779336227a7782b14e3028cf2b727429c0c
Govindan Gopikrishnan <govindan.gopikrishnan@tesco.com>
2021-10-05 11:55:39 +0530

    updating the added content

--- src/Sections/DosAndDonts/index.js
+++ src/Sections/DosAndDonts/index.js
@@ -1,60 +1,74 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import { dos, donts } from './data';
 import styles from './styles.module.scss';
+import doSvg from './assets/dos.svg';
+import dontSvg from './assets/donts.svg';
 
 const DosAndDonts = () => {
   return (
     <Container fluid>
-      <Row className={styles.dosAndDontsWrapper}>
+      <Row  className={styles.dosAndDontsWrapper}>
         <Col>
-          <h5>
-            Do's
-          </h5>
-          <ul>
-            {
-              dos.map(({
-                text,
-                linkText,
-                link
-              }) => (
-                <li>
-                  {text + ' '}
-                  {
-                    linkText && link ? (
-                      <a href={link}>
-                        {linkText}
-                      </a>
-                    ) : ''
-                  }
-                </li>
-              ))
-            }
-          </ul>
+          <div className={styles.dosDontsContent}>
+            <h5>
+              Do's
+            </h5>
+            <ul>
+              {
+                dos.map(({
+                  text,
+                  linkText,
+                  link
+                }) => (
+                  <li>
+                    {text + ' '}
+                    {
+                      linkText && link ? (
+                        <a href={link}>
+                          {linkText}
+                        </a>
+                      ) : ''
+                    }
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </Col>
+        <Col className="text-center">
+          <Image src={doSvg} fluid/>
+        </Col>
+      </Row>
+      <Row className={styles.dosAndDontsWrapper}>
+        <Col className="text-center">
+          <Image src={dontSvg} fluid/>
         </Col>
         <Col>
-          <h5>
-            Dont's
-          </h5>
-          <ul>
-            {
-              donts.map(({
-                text,
-                linkText,
-                link
-              }) => (
-                <li>
-                  {text + ' '}
-                  {
-                    linkText && link ? (
-                      <a href={link}>
-                        {linkText}
-                      </a>
-                    ) : ''
-                  }
-                </li>
-              ))
-            }
-          </ul>
+          <div className={styles.dosDontsContent}>
+            <h5>
+              Dont's
+            </h5>
+            <ul>
+              {
+                donts.map(({
+                  text,
+                  linkText,
+                  link
+                }) => (
+                  <li>
+                    {text + ' '}
+                    {
+                      linkText && link ? (
+                        <a href={link}>
+                          {linkText}
+                        </a>
+                      ) : ''
+                    }
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
         </Col>
       </Row>
     </Container>
